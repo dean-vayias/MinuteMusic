adbae6a4fc4674f018cc5e1f771626451bf20316
Dean Vayias <deanvayias1024@gmail.com>
2023-04-12 22:20:56 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Dean Vayias on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
